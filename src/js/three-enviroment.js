f15978ba841f892f157dbebbd81f76a37ed97955
chripak <chripak@shoptet.cz>
2022-05-23 10:30:10 +0200

    initial commit

--- src/js/three-enviroment.js
+++ src/js/three-enviroment.js
@@ -1,53 +1,84 @@
 import {
 	Scene,
 	PerspectiveCamera,
-	MeshPhongMaterial,
 	WebGLRenderer,
 	SpotLight,
-	SpotLightHelper,
-	SphereGeometry,
-	Mesh
+	AmbientLight,
+	Fog
   } from 'three';
 
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import OBJShoe from '../models/Blue_Vans_Shoe_SF.obj';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 
 	const container = document.querySelector('#three-container');
 
 	const scene = new Scene();
 		
 	const camera = new PerspectiveCamera( 45, container.offsetWidth / container.offsetHeight, 0.1, 1000 );
-		camera.position.z = 30;
+		camera.position.z = 18;
+		camera.position.y = 1;
 		
 	const renderer = new WebGLRenderer({antialias : true, alpha: true});
 		renderer.setSize( container.offsetWidth, container.offsetHeight );
 		renderer.setClearColor(0x000000, 0);
 		container.appendChild(renderer.domElement);
 		
-	const controls = new OrbitControls(camera, renderer.domElement);
-
-
+	scene.fog = new Fog( 0xffffff, 15, 30 );
 
 	//SpotLight -------------------------------------------------------------------------------------
-	var colorLight = 0xAED5FF,
-		intensityLight = 1,
+	var colorLight = 0xffffff,
+		intensityLight = 0.2,
 		distanceLight = 0,
 		angleLight = Math.PI / 2,
 		penumbraLight = 1;
 
 	const spotLight = new SpotLight(colorLight, intensityLight, distanceLight, angleLight, penumbraLight);
-		spotLight.position.set(5, 5, 10);
-		
+		spotLight.position.set(50, 50, 100);
 		scene.add(spotLight);	
 
-	const spotLightHelper = new SpotLightHelper(spotLight);
-		scene.add(spotLightHelper);
+	const spotLightBlue = new SpotLight(0x004eff, 1, distanceLight, angleLight, penumbraLight);
+		spotLightBlue.position.set(-100, 100, 0);
+		scene.add(spotLightBlue);	
 
-	//Sphere ----------------------------------------------------------------------------------------
-	const sphereGeometry = new SphereGeometry( 5, 32, 32 );
-	const sphereMaterial = new MeshPhongMaterial( {color: 0xffff00,wireframe: true} );
-	const sphere = new Mesh( sphereGeometry, sphereMaterial );
-	scene.add( sphere );
+	const spotLightYellow = new SpotLight(0xfbff00, 0.3, distanceLight, angleLight, penumbraLight);
+		spotLightYellow.position.set(100, 100, 0);
+		scene.add(spotLightYellow);	
 
+	const ambientLight = new AmbientLight( 0x404040, 1.4 ); // soft white light
+		scene.add( ambientLight );
+
+	//Shoes ----------------------------------------------------------------------------------------
+	const loader = new OBJLoader();
+  	let shoeModels = [];
+	// load a resource
+	loader.load(
+		OBJShoe,
+		function ( object ) {
+			let amount = 6;
+			for (let i = 0; i < amount; i++) {
+				const shoeClone = object.clone();
+				shoeClone.rotation.x = 0.45;
+				shoeClone.rotation.y = 0.45;
+				scene.add(shoeClone);
+				shoeModels.push(shoeClone);
+			}
+			//remove setInterval
+			/*
+			let counter = 0;
+			let addShoues = setInterval(function(){
+				shoeModel[counter].rotation.x = 0.45;
+				shoeModel[counter].rotation.y = 0.45;
+				counter++
+				if (counter==amount) {
+					clearInterval(addShoues);
+				}
+			},100)*/
+		},
+		// called when loading is in progresses
+		function ( xhr ) {console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );},
+		// called when loading has errors
+		function ( error ) {console.log( 'An error happened' );}
+	);
 
 	//Resize ----------------------------------------------------------------------------------------
 	window.addEventListener( 'resize', function(){
@@ -56,35 +87,26 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 		renderer.setSize(container.offsetWidth,container.offsetHeight);
 		renderer.render(scene, camera);
 	}, false );
-
-
-
-	// Animations ----------------------------------------------------------------------------------
-
-	document.querySelector('#b1').addEventListener("click", function(){
-		sphereTo.position.x = 0;
-	});
-	document.querySelector('#b2').addEventListener("click", function(){
-		sphereTo.position.x = 10;
+	
+	let mousePositionX = 0;
+	document.addEventListener("mousemove", function(event){
+		//recalculate position from center of viewport
+		mousePositionX = event.clientX/500;
 	});
 
-	/*speed = update position by xx% of the distance between position and target position*/
-  
-	var sphereTo = {
-		position:{x:0,y:0,z:0},
-		speed: 0.005,
+	function lerp(start, end, amt){
+		return (1-amt)*start+amt*end;
 	};
 
-
 	function animate(){
-		//lerp
-		sphere.position.x += (sphereTo.position.x - sphere.position.x)*sphereTo.speed;
-		sphere.position.y += (sphereTo.position.y - sphere.position.y)*sphereTo.speed;
-		sphere.position.z += (sphereTo.position.z - sphere.position.z)*sphereTo.speed;
-
-		sphere.rotation.y += 0.005;
-
-		controls.update();
+		if (shoeModels.length > 0) {
+			for (let index = 0; index < shoeModels.length; index++) {
+				const model = shoeModels[index];
+				// use Lerp function for smooth animation 
+				model.position.x = 10*Math.cos(mousePositionX+index);
+				model.position.z = 10*Math.sin(mousePositionX+index);
+			}
+		}
 		renderer.render(scene, camera);
 		requestAnimationFrame(animate);
 	};
